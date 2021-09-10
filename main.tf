2367b923e5f56e37ffc7aefeb4bd2a79069fa5d3
Yulei Liu <yulei.liu@gmail.com>
2021-09-10 18:49:48 +1000

    add provider block

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 ##main.tf
+provider "tfe" {
+  ##Please add your TFE token to TFE_TOKEN enviroment variable. 
+  ##This token need to have permission to read all managed workspaces and write variables
+  ## By default this provider manages TFC, to manage an TFE enviroment, set TFE_HOSTNAME envirioment variable to the hostname of you TFE.
+}
 
 data "tfe_workspace_ids" "customerfacing" {
   tag_names    = ["customerfacing"]
